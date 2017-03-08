382893643662852453750f3c3a9ec58d9fa2e61d
Max Cai <zhiyuan.cai@gmail.com>
2017-03-08 14:42:13 -0500

    initial commit

---
+++ send.py
@@ -0,0 +1,8 @@
+import socket
+
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 5007
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+sock.sendto("ping from: " + socket.gethostname(), (MCAST_GRP, MCAST_PORT))
